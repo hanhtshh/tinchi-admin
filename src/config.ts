705450ccf7610caa9529c032f59581c12128520c
Nguyen Van Hanh <nguyenhanh3@telio.vn>
2023-05-09 13:34:00 +0700

    ok

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ interface IConfig {
 }
 
 const config = <IConfig>{
-  service_host: process.env.NEXT_PUBLIC_SERVICE_HOST,
+  service_host: process.env.NEXT_PUBLIC_SERVICE_HOST || "https://tinchi.onrender.com/tinchi",
 };
 
 export default config;
